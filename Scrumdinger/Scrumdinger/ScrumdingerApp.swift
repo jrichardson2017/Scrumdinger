267afe4771d0137a349a41b55f25611062abd67e
Jrichardson <jkgrichardson@gmail.com>
2024-10-10 22:11:07 -0400

    Initial commit

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by justin richardson on 2024-10-10.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
