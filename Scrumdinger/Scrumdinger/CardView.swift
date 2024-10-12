39954476ef9bb9526fc797b25212814a769d7095
Jrichardson <jkgrichardson@gmail.com>
2024-10-11 21:05:30 -0400

    create the card view, part 1

---
+++ Scrumdinger/Scrumdinger/CardView.swift
@@ -0,0 +1,25 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by justin richardson on 2024-10-11.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.mockData[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(scrum.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
