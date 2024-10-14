dff09e3266e9eb8ea5f20bd2e47e3f3c40c1acbe
jkgrichardson-mbp <jkgrichardson@gmail.com>
2024-10-13 22:15:41 -0400

    Replace the default Text view with a List view. Pass the scrums array and a key path to the title property to the initializer.

--- Scrumdinger/Scrumdinger/ScrumsView.swift
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -11,7 +11,9 @@ struct ScrumsView: View {
     let scrums: [DailyScrum]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(scrums, id: \.title) { scrum in
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Content@*/Text("Content")/*@END_MENU_TOKEN@*/
+        }
     }
 }
 
