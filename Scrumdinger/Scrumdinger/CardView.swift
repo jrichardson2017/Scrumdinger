fc59fdc8c33ec04a24cecd12d1f200397f38a702
jkgrichardson-mbp <jkgrichardson@gmail.com>
2024-10-13 21:09:57 -0400

    Add a similar accessibility modifier to the second Label in the HStack.

--- Scrumdinger/Scrumdinger/CardView.swift
+++ Scrumdinger/Scrumdinger/CardView.swift
@@ -24,6 +24,7 @@ struct CardView: View {
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
                     .labelStyle(.trailingIcon)
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
             }
             .font(.caption)
         }
