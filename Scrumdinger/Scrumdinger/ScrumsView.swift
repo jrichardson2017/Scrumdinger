e976ff5e46e59858015620b6472a21b199c2a8ae
jkgrichardson-mbp <jkgrichardson@gmail.com>
2024-10-13 22:01:58 -0400

    Add a constant named scrums of type [DailyScrum] and pass an array of scrums to the ScrumsView preview.

--- Scrumdinger/Scrumdinger/ScrumsView.swift
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ScrumsView()
+    ScrumsView(scrums: DailyScrum.mockData)
 }
