5ea83b271d54ba6bfaafb1e97cbdab26c60fa53f
jkgrichardson-mbp <jkgrichardson@gmail.com>
2024-10-13 21:45:16 -0400

    Create a new SwiftUI View file named ScrumsView

---
+++ Scrumdinger/Scrumdinger/ScrumsView.swift
@@ -0,0 +1,18 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by justin richardson on 2024-10-13.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrumsView()
+}
