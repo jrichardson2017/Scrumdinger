ef21bcf97a7d8e8cd052bf17ed4692352d99d2d4
jkgrichardson-mbp <jkgrichardson@gmail.com>
2024-10-16 11:49:37 -0400

    Add a Theme row to for the theme color of the Scrum

--- Scrumdinger/Scrumdinger/DetailView.swift
+++ Scrumdinger/Scrumdinger/DetailView.swift
@@ -22,7 +22,15 @@ struct DetailView: View {
                 Text("\(scrum.lengthInMinutes) minutes")
             }
             .accessibilityElement(children: .combine)
-            
+            HStack {
+                Label("Theme", systemImage: "paintpalette")
+                Spacer()
+                Text("\(scrum.theme.name)")
+                    .padding(4)
+                    .foregroundStyle(scrum.theme.accentColor)
+                    .background(scrum.theme.mainColor)
+                    .cornerRadius(4)
+            }
         }
     }
 }
