bd0de4fba60ba5fcf647226c8a24f6f74d14d3d7
jkgrichardson-mbp <jkgrichardson@gmail.com>
2024-10-16 12:06:26 -0400

    Craete a new section that displays the DailyScrum Attendee objects

--- Scrumdinger/Scrumdinger/DetailView.swift
+++ Scrumdinger/Scrumdinger/DetailView.swift
@@ -12,24 +12,31 @@ struct DetailView: View {
     
     var body: some View {
         List {
-            Section(header: Text("Meeting Info")) { }
-            Label("Start Timer", systemImage: "timer")
-                .font(.headline)
-                .foregroundStyle(Color.accentColor)
-            HStack {
-                Label("Length", systemImage: "timer")
-                Spacer()
-                Text("\(scrum.lengthInMinutes) minutes")
+            Section(header: Text("Meeting Info")) {
+                Label("Start Timer", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundStyle(Color.accentColor)
+                HStack {
+                    Label("Length", systemImage: "timer")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text("\(scrum.theme.name)")
+                        .padding(4)
+                        .foregroundStyle(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
             }
-            .accessibilityElement(children: .combine)
-            HStack {
-                Label("Theme", systemImage: "paintpalette")
-                Spacer()
-                Text("\(scrum.theme.name)")
-                    .padding(4)
-                    .foregroundStyle(scrum.theme.accentColor)
-                    .background(scrum.theme.mainColor)
-                    .cornerRadius(4)
+            
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label("\(attendee.name)", systemImage: "person")
+                }
             }
         }
     }
