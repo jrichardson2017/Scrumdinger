7130af8929dbb6c1423113a406908b26d29ed3c2
jkgrichardson-mbp <jkgrichardson@gmail.com>
2024-10-16 21:12:11 -0400

    add accessibility modifiers to help VoiceOver users navigate the controls

--- Scrumdinger/Scrumdinger/DetailEditView.swift
+++ Scrumdinger/Scrumdinger/DetailEditView.swift
@@ -16,11 +16,12 @@ struct DetailEditView: View {
             Section(header: Text("Meeting Info")) {
                 TextField("Title", text: $scrum.title)
                 HStack {
-                    Slider(value: $scrum.lengthInMinutesAsDouble,
-                           in: 5...30,
-                           step: 1) {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
                         Text("Length")
+                            .accessibilityHidden(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                            
                     }
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minutes")
                     Spacer()
                     Text("\(scrum.lengthInMinutes) minutes")
                 }
@@ -42,6 +43,7 @@ struct DetailEditView: View {
                         newAttendeeName = ""
                     }) {
                         Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("add attendee")
                     }
                     .disabled(newAttendeeName.isEmpty)
                 }
