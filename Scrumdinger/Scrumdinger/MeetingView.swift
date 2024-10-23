f62cc7e97df646e4be0fde57e4bf7827569fed92
jkgrichardson-mbp <jkgrichardson@gmail.com>
2024-10-22 21:40:18 -0400

    Add a state object for a source of truth

--- Scrumdinger/Scrumdinger/MeetingView.swift
+++ Scrumdinger/Scrumdinger/MeetingView.swift
@@ -9,36 +9,22 @@ import SwiftUI
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
     
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 16)
                 .fill(scrum.theme.mainColor)
             VStack {
-                ProgressView(value: 5, total: 15)
+                MeetingHeaderView(
+                    secondsElapsed: scrumTimer.secondsElapsed,
+                    secondsRemaining: scrumTimer.secondsRemaining,
+                    theme: scrum.theme
+                )
                 
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Seconds Elapsed")
-                            .font(.caption)
-                        Label("300", systemImage: "hourglass.tophalf.fill")
-                    }
-                    
-                    Spacer()
-                    
-                    VStack(alignment: .trailing) {
-                        Text("Seconds Remaining")
-                            .font(.caption)
-                        Label("600", systemImage: "hourglass.bottomhalf.fill")
-                    }
-                }
-                .accessibilityElement(children: /*@START_MENU_TOKEN@*/.ignore/*@END_MENU_TOKEN@*/)
-                .accessibilityLabel("Time Remaining")
-                .accessibilityValue("10 minutes")
-
                 Circle()
                     .strokeBorder(lineWidth: 24)
-                    
+                
                 
                 HStack {
                     Text("Speaker 1 of 3")
---
+++ Scrumdinger/Scrumdinger/Models/ScrumTimer.swift
@@ -0,0 +1,15 @@
+//
+//  ScrumTimer.swift
+//  Scrumdinger
+//
+//  Created by justin richardson on 2024-10-22.
+//
+import Foundation
+
+@MainActor
+final class ScrumTimer: ObservableObject {
+    
+    var secondsElapsed = 0
+    var secondsRemaining = 0
+    
+}
