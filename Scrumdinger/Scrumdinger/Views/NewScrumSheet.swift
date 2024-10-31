81fb59b22b444b20e3939008f3b2fd493574b9b0
jkgrichardson-mbp <jkgrichardson@gmail.com>
2024-10-30 22:05:39 -0400

    Show a NewScrumSheet to the user when they tap the Add button on the ScrumsView

---
+++ Scrumdinger/Scrumdinger/Views/NewScrumSheet.swift
@@ -0,0 +1,18 @@
+//
+//  NewScrumSheet.swift
+//  Scrumdinger
+//
+//  Created by justin richardson on 2024-10-30.
+//
+
+import SwiftUI
+
+struct NewScrumSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewScrumSheet()
+}
